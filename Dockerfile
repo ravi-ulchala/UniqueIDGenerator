4e28214045b2629806d9a353c14f6a65a3f8c9c3
rulchala <rulchala@helix-int.com>
2024-07-24 16:06:30 +0530

    ValueLabs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM openjdk:11-jre-slim
+FROM openjdk:17-jdk-slim
 WORKDIR /app
 COPY target/Tracking-0.0.jar app.jar
-EXPOSE 6666
+EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/labs/Tracking/service/TrackingNumberService.java
+++ src/main/java/com/labs/Tracking/service/TrackingNumberService.java
@@ -5,7 +5,8 @@ import com.labs.Tracking.repository.TrackingNumberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 @Service
 public class TrackingNumberService {
 
@@ -17,6 +18,10 @@ public class TrackingNumberService {
     }
 
     @Transactional
+    @Retryable(
+            value = { RuntimeException.class },
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 2000))
     public TrackingNumber generateTrackingNumber() {
         TrackingNumber trackingNumber = new TrackingNumber();
         return repository.save(trackingNumber);
